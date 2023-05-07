25824c80a10004de977d09b426184561b0bdcd59
Lucas Safeno <lugnorio@gmail.com>
2023-05-07 17:45:12 -0300

    FrontStart - VueJS - v-for

---
+++ frontstart/06-vueJS/04-v-for/index.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+    /*  list:[
+        {id:1, name: 'Lucas'},
+        {id:2, name: 'Thauanna'},
+        {id:3, name: 'Daniel'},
+        {id:4, name: 'Zé'},
+      ] */
+      list:{
+        name: 'Lucas',
+        age: 27,
+        graduation: 'eng comp'
+      }
+    }
+  })
+
