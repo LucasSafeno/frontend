db632c81dee306bc3b5e8dc6bbd9a9b201f52116
Lucas Safeno <lugnorio@gmail.com>
2023-05-08 21:49:44 -0300

    Frontstart - VueJS - Projeto : Todo-List

---
+++ frontstart/projetos/01-todolist-vue/todolist/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <TodoList />
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app {
+  padding: 2em;
+  font-weight: normal;
+
+}
+</style>
